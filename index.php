7f39a17fba3aa81b13a61bec0bc4ae8762037131
Nightfall <marcelantonio246@gmail.com>
2023-05-02 12:20:32 +0700

    add styling & edit admin page

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+header('location:main/file/admin_page.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>INDEX</title>
+
+</head>
+<body>
+   
+
+</body>
+</html>
