c29a7385c22306ec6e7ad165b6a2f3d1ccfc6578
Nightfall <marcelantonio246@gmail.com>
2023-07-21 10:41:26 +0700

    new login form

--- login_form.php
+++ login_form.php
@@ -41,125 +41,73 @@ if(isset($_POST['submit'])){
 
 };
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <title>Login</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="description" content="Admin template that can be used to build dashboards for CRM, CMS, etc." />
-    <meta name="author" content="Potenza Global Solutions" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <!-- app favicon -->
-    <link rel="shortcut icon" href="assets/img/favicon.ico">
-    <!-- google fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
-    <!-- plugin stylesheets -->
-    <link rel="stylesheet" type="text/css" href="assets/css/vendors.css" />
-    <!-- app style -->
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
-</head>
-
-<body class="bg-white">
-    <!-- begin app -->
-    <div class="app">
-        <!-- begin app-wrap -->
-        <div class="app-wrap">
-            <!-- begin pre-loader -->
-            <div class="loader">
-                <div class="h-100 d-flex justify-content-center">
-                    <div class="align-self-center">
-                        <img src="assets/img/loader/loader.svg" alt="loader">
+    <head>
+        <link rel="stylesheet" href="components/login.css">
+        <title>Login</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="description" content="Admin template that can be used to build dashboards for CRM, CMS, etc." />
+        <meta name="author" content="Potenza Global Solutions" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <!-- app favicon -->
+        <link rel="shortcut icon" href="assets/img/favicon.ico">
+        <!-- google fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+        <!-- plugin stylesheets -->
+        <link rel="stylesheet" type="text/css" href="assets/css/vendors.css" />
+        <!-- app style -->
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="left">
+                <div id="login">
+                    <div class="logo">
+                        <img src="components/logos.png" alt="">
                     </div>
-                </div>
-            </div>
-            <!-- end pre-loader -->
-
-            <!--start login contant-->
-            <div class="app-contant">
-                <div class="bg-white">
-                    <div class="container-fluid p-0">
-                        <div class="row no-gutters">
-                            <div class="col-sm-6 col-lg-5 col-xxl-3  align-self-center order-2 order-sm-1">
-                                <div class="d-flex align-items-center h-100-vh">
-                                    <div class="login p-50">
-                                        <h1 class="mb-2">We Are Mentor</h1>
-                                        <p>Welcome back, please login to your account.</p>
-                                        <form action="" method="post" class="mt-3 mt-sm-5">
-                                            <?php
-                                              if(isset($error)){
-                                                 foreach($error as $error){
-                                                    echo '<div class="col-12">
-                                                            <div class="alert alert-danger alert-icon" role="alert">
-                                                                <i class="fa fa-info-circle"></i> Username or Password Wrong!
-                                                            </div>
-                                                        </div>';
-                                                    // echo '<span class="error-msg">'.$error.'</span>';
-                                                 };
-                                              };
-                                            ?>
-                                            <div class="row">
-                                                <div class="col-12">
-                                                    <div class="form-group">
-                                                        <label class="control-label">Email*</label>
-                                                        <input type="email" name="email" class="form-control" required placeholder="enter your email" />
-                                                    </div>
-                                                </div>
-                                                <div class="col-12">
-                                                    <div class="form-group">
-                                                        <label class="control-label">Password*</label>
-                                                        <input type="password" name="password" class="form-control" required placeholder="enter your password" />
-                                                    </div>
-                                                </div>
-                                                <div class="col-12">
-                                                    <div class="d-block d-sm-flex  align-items-center">
-                                                        <div class="form-check">
-                                                            <input class="form-check-input" type="checkbox" id="gridCheck">
-                                                            <label class="form-check-label" for="gridCheck">
-                                                                Remember Me
-                                                            </label>
-                                                        </div>
-                                                        <a href="javascript:void(0);" class="ml-auto">Forgot Password ?</a>
-                                                    </div>
-                                                </div>
-                                                <div class="col-12 mt-3">
-                                                    <input type="submit" name="submit" value="LOGIN" class="btn btn-primary text-uppercase">
-                                                    
-                                                </div>
-                                                <div class="col-12  mt-3">
-                                                    <p>Don't have an account ?<a href="register_form.php"> Sign Up</a></p>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-6 col-xxl-9 col-lg-7 bg-gradient o-hidden order-1 order-sm-2">
-                                <div class="row align-items-center h-100">
-                                    <div class="col-7 mx-auto ">
-                                        <img class="img-fluid" src="assets/img/bg/login.svg" alt="">
-                                    </div>
-                                </div>
-                            </div>
+                    <div class="label">
+                        <h1>PT Jaringan Inklusi Keuangan</h1>
+                    </div>
+                    
+                    <form action="" method="post">
+                        <?php
+                          if(isset($error)){
+                             foreach($error as $error){
+                                echo '<div class="col-12">
+                                        <div class="alert alert-danger alert-icon" role="alert">
+                                            <i class="fa fa-info-circle"></i> Username or Password Wrong!
+                                        </div>
+                                    </div>';
+                                // echo '<span class="error-msg">'.$error.'</span>';
+                             };
+                          };
+                        ?>
+                        <div>
+                            <label>Email</label>
+                            <input type="email" name="email" class="text-input form-control" required placeholder="Enter your email" />
                         </div>
+                        <div>
+                            <label>Password</label>
+                            <input type="password" name="password" class="text-input form-control" required placeholder="Enter your password" />
+                        </div>
+                        <button type="submit" class="primary-btn" name="submit" value="LOGIN">Login</button>
+                    </form>
+                </div>
+            </div>
+            <div id="right">
+                <div id="image">
+                    <div class="image-content">
+                        <h1 class="image-text">Selamat Datang <strong>Contoh</strong></h1>
                     </div>
                 </div>
             </div>
-            <!--end login contant-->
         </div>
-        <!-- end app-wrap -->
-    </div>
-    <!-- end app -->
-
-
-
-    <!-- plugins -->
+            <!-- plugins -->
     <script src="assets/js/vendors.js"></script>
 
     <!-- custom app -->
     <script src="assets/js/app.js"></script>
-</body>
-
-
+    </body>
 </html>
